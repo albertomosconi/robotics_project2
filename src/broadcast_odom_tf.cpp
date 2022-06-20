805f50bca0085191a19512a80ecde8bae9ea7a94
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2022-06-20 21:09:11 +0200

    update gmapping and node configuration

--- src/broadcast_odom_tf.cpp
+++ src/broadcast_odom_tf.cpp
@@ -2,27 +2,31 @@
 #include <tf2_ros/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 
-#define TOPIC_ROBOT_ODOM "odom"
-#define TF_FRAME_PARENT "odom"
-#define TF_FRAME_CHILD "base_link"
+#define EXE_NAME "broadcast_odom_tf"
 #define QUEUE_SIZE 1000
 
 void robot_odom_callback(tf2_ros::TransformBroadcaster *broadcaster, geometry_msgs::TransformStamped *message, const nav_msgs::OdometryConstPtr &odom);
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "odom_tf");
-
+    // initialize node
+    ros::init(argc, argv, EXE_NAME);
     ros::NodeHandle n;
+
+    // read parameters
+    std::string TF_PARENT, TF_CHILD, ODOM_TOPIC;
+    n.param<std::string>(EXE_NAME "/parent_frame", TF_PARENT, "world");
+    n.param<std::string>(EXE_NAME "/child_frame", TF_CHILD, "robot");
+    n.param<std::string>(EXE_NAME "/odom_topic", ODOM_TOPIC, "odom");
 
     ros::Subscriber sub_odom;
     tf2_ros::TransformBroadcaster tf_odom_broadcaster;
     geometry_msgs::TransformStamped tf_message_odom;
-    tf_message_odom.header.frame_id = TF_FRAME_PARENT;
-    tf_message_odom.child_frame_id = TF_FRAME_CHILD;
+    tf_message_odom.header.frame_id = TF_PARENT;
+    tf_message_odom.child_frame_id = TF_CHILD;
 
     sub_odom = n.subscribe<nav_msgs::Odometry>(
-        TOPIC_ROBOT_ODOM,
+        ODOM_TOPIC,
         QUEUE_SIZE,
         boost::bind(&robot_odom_callback, &tf_odom_broadcaster, &tf_message_odom, _1));
 
@@ -38,10 +42,7 @@ void robot_odom_callback(tf2_ros::TransformBroadcaster *broadcaster, geometry_ms
     message->transform.translation.x = odom->pose.pose.position.x;
     message->transform.translation.y = odom->pose.pose.position.y;
     message->transform.translation.z = odom->pose.pose.position.z;
-    message->transform.rotation.x = odom->pose.pose.orientation.x;
-    message->transform.rotation.y = odom->pose.pose.orientation.y;
-    message->transform.rotation.z = odom->pose.pose.orientation.z;
-    message->transform.rotation.w = odom->pose.pose.orientation.w;
+    message->transform.rotation = odom->pose.pose.orientation;
 
     (*broadcaster).sendTransform(*message);
 }
